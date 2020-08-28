4e7bfd29fed225955cfd1733e4eb82873cc8e960
hacene92230 <33603117+hacene92230@users.noreply.github.com>
2020-08-28 13:11:15 +0200

    modification des ventes avec un affichage corriger

--- vente.php
+++ vente.php
@@ -1,4 +1,4 @@
-<table>
+table>
         <caption>Vendre</caption>
         <thead>
             <tr>
@@ -8,15 +8,17 @@
             </tr>
         </thead>
     <tr>
+    
     <?php
-$sql = "select * from produits where pro_type = :type";
-$statement = $link->prepare($sql);
-$statement->execute([":type" => $_GET['action']]);
-foreach($statement->fetch() as $cle)
-{
-        echo "<td>$cle</td>";
-}
-
-?>
-</tr>
+        $sql = "select * from produits where pro_type = :type";
+        $statement = $link->prepare($sql);
+        $statement->execute([":type" => $_GET['action']]);
+        foreach ($statement->fetchAll() as $cle) {
+            echo "<td>" . $cle['pro_numero'] . "</td>";
+            echo "<td>" . $cle['pro_type'] . "</td>";
+            echo "<td>" . $cle['pro_adresse'] . "</td>";
+            echo "<td>" . $cle['pro_carac'] . "</td>";
+            echo "</tr>";
+        }
+        ?>
 </table>
