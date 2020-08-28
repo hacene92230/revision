070c0e94373b820f8904e5201b37210c8aac22c2
hacene92230 <33603117+hacene92230@users.noreply.github.com>
2020-08-28 13:22:01 +0200

    je vient de commencer le traitement du formulaire de contact

--- vente.php
+++ vente.php
@@ -1,23 +1,21 @@
-table>
-        <caption>Vendre</caption>
-        <thead>
-            <tr>
-                                                                <th>type du bien</th>
-                <th>Caractéristique</th>
-                <th>Adresse</th>
-            </tr>
-        </thead>
+<table>
+    <caption>Vendre</caption>
+    <thead>
+        <tr>
+            <th>type du bien</th>
+            <th>Caractéristique</th>
+            <th>Adresse</th>
+        </tr>
+    </thead>
     <tr>
-    
-    <?php
+        <?php
         $sql = "select * from produits where pro_type = :type";
         $statement = $link->prepare($sql);
         $statement->execute([":type" => $_GET['action']]);
         foreach ($statement->fetchAll() as $cle) {
-            echo "<td>" . $cle['pro_numero'] . "</td>";
             echo "<td>" . $cle['pro_type'] . "</td>";
-            echo "<td>" . $cle['pro_adresse'] . "</td>";
             echo "<td>" . $cle['pro_carac'] . "</td>";
+            echo "<td>" . $cle['pro_adresse'] . "</td>";
             echo "</tr>";
         }
         ?>
