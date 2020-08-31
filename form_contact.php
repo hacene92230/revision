45d45f590d39a2dbf3f6b237cfac069e457c7d9c
hacene92230 <33603117+hacene92230@users.noreply.github.com>
2020-08-31 09:10:27 +0200

    ajout de l'enregistrement en bdd de nouveau clients en bdd

--- form_contact.php
+++ form_contact.php
@@ -1,5 +1,19 @@
 <?php
 include "header.php";
+if(isset($_POST['btContact']))
+{
+    extract($_POST);
+    $sql = "insert into clients values (null,:cli_prenom, :cli_nom, :cli_adresse, :cli_ville, :cli_postal)";
+$statement = $link->prepare($sql);
+$statement->execute([
+            ":cli_prenom" => $cli_prenom,
+    ":cli_nom" => $cli_nom,
+    ":cli_adresse" => $cli_adresse,
+    ":cli_ville" => $cli_ville,
+    ":cli_postal" => $cli_postal
+]);
+echo "<h2>Le client à bien été ajouter</h2>";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,29 +36,41 @@ include "header.php";
             </tr>
         </thead>
     <tr>
-</tr>
-</table>
+    <?php
+        $sql = "select * from clients";
+        $statement = $link->prepare($sql);
+        $statement->execute();
+        foreach ($statement->fetchAll() as $cle) {
+            echo "<td>" . $cle['cli_nom'] . "</td>";
+            echo "<td>" . $cle['cli_prenom'] . "</td>";
+            echo "<td>" . $cle['cli_adresse'] . "</td>";
+            echo "<td>" . $cle['cli_ville'] . "</td>";
+            echo "<td>" . $cle['cli_postal'] . "</td>";
+            echo "</tr>";
+        }
+?>
+        </table>
     <h1>client</h1>
-    <form action="contact_traitement.php" method="POST">
+    <form action="" method="POST">
         <p>
             <label for="nom">Nom de famille</label>
-            <input type=" text" id="nom" name="nom" placeholder="Saisir votre nom de famille" size="50" />
+            <input type=" text" id="nom" name="cli_nom" placeholder="Saisir votre nom de famille" size="50" />
         </p>
         <p>
             <label for="prenom">Prénom</label>
-            <input type="text" id="prenom" name="prenom" placeholder="Saisir votre prénom" size="50" />
+            <input type="text" id="prenom" name="cli_prenom" placeholder="Saisir votre prénom" size="50" />
         </p>
         <p>
             <label for="adresse">Adresse de résidance</label>
-            <input type="text" id="adresse" name="adresse" placeholder="Saisir votre adresse" size="50" />
+            <input type="text" id="adresse" name="cli_adresse" placeholder="Saisir votre adresse" size="50" />
         </p>
         <p>
             <label for="ville">La ville ou vous résidez</label>
-            <input type="text" id="ville" name="ville" placeholder="Saisir votre ville" size="50" />
+            <input type="text" id="ville" name="cli_ville" placeholder="Saisir votre ville" size="50" />
         </p>
         <p>
             <label for="cp">Saisir votre code postal à 5 chiffres.</label>
-            <input type="number" id="cp" name="cp" placeholder="Code postal" size="50" />
+            <input type="number" id="cp" name="cli_postal" placeholder="Code postal" size="50" />
         </p>
         <input type="submit" name="btContact" value="Ajouter un client" />
             </form>
